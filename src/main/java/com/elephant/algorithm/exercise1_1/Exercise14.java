c5a0e9bbe1d3f4998cd0c3c680154eeb3581d26f
newbeess <newbeessfu@gmail.com>
2016-04-25 21:54:56 +0800

    complete Exercise14

---
+++ src/main/java/com/elephant/algorithm/exercise1_1/Exercise14.java
@@ -0,0 +1,17 @@
+package com.elephant.algorithm.exercise1_1;
+
+/**
+ * Created by elephant on 16/4/25.
+ */
+public class Exercise14 {
+	static Integer lg(Integer N){
+		int res=0;
+		for (int i=N;i>=2;i/=2)
+			res++;
+		return res;
+	}
+	public static void main(String[] args){
+		int x=10;
+		System.out.println(lg(x));
+	}
+}
