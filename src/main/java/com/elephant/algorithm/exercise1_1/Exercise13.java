4792c454098318c4137471ae0f193917b25abf96
newbeess <newbeessfu@gmail.com>
2016-04-25 20:08:18 +0800

    add all

---
+++ src/main/java/com/elephant/algorithm/exercise1_1/Exercise13.java
@@ -0,0 +1,7 @@
+package com.elephant.algorithm.exercise1_1;
+
+/**
+ * Created by elephant on 16/4/25.
+ */
+public class Exercise13 {
+}
---
+++ src/main/java/com/elephant/algorithms/Test.java
@@ -0,0 +1,17 @@
+package com.elephant.algorithms;
+
+import static com.elephant.util.Print.*;
+
+/**
+ * Created by elephant on 16/4/24.
+ */
+public class Test {
+	public static void main(String[] args) {
+		int x=-1234567891;
+		 print(Math.abs(x));
+		//print(Math.abs(-12345678912));
+		double y=Double.POSITIVE_INFINITY;
+		print(y);
+		print(1/0.0);
+	}
+}
