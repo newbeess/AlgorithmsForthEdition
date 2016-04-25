7bf3f7f5fcbe79f6ff4f37bb0af4844d333c168f
newbeess <newbeessfu@gmail.com>
2016-04-25 09:03:17 +0800

    complete Exercise9

--- src/main/java/com/elephant/algorithm/exercise1_1/Exercise8.java
+++ src/main/java/com/elephant/algorithm/exercise1_1/Exercise8.java
@@ -1,12 +1,13 @@
 package com.elephant.algorithm.exercise1_1;
 
 /**
- * Created by elephant on 16/4/24.
+ * String override +
  */
 public class Exercise8 {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println('b');
-		System.out.println('b'+'c');
-		System.out.println((char)('a'+4));
+		System.out.println('b' + 'c');
+		System.out.println("b" + "c");
+		System.out.println((char) ('a' + 4));
 	}
 }
---
+++ src/main/java/com/elephant/algorithm/exercise1_1/Exercise9.java
@@ -0,0 +1,15 @@
+package com.elephant.algorithm.exercise1_1;
+
+/**
+ * Created by elephant on 16/4/25.
+ */
+public class Exercise9 {
+
+	public static void main(String[] args){
+		int N=10;
+		String s="";
+		for (int n=N;n>0;n/=2)
+			s=(n%2)+s;
+		System.out.println(s);
+	}
+}
