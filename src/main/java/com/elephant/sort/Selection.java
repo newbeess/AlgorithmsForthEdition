846f24055231287540f74a22fd010dbb5e3e66d3
newbeess <newbeessfu@gmail.com>
2016-05-30 10:25:55 +0800

    modify

--- src/main/java/com/elephant/sort/Selection.java
+++ src/main/java/com/elephant/sort/Selection.java
@@ -13,7 +13,7 @@ public class Selection extends Example {
 	public void sort(Comparable[] a) {
 		int N = a.length;
 		for (int i = 0; i < N; i++) {
-			int min = 0;
+			int min = i;
 			for (int j = i + 1; j < N; j++) {
 				if (less(a[j], a[min]))
 					min = j;
